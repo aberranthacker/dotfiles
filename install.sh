caa90f183008f65cddd5624f139947b9293022c3
Oleg Tsymbalyuk <randomizer@outlook.com>
2016-05-20 06:03:16 +0000

    remove vim/bundle dir from repo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm ~/.vim
+
 rm ~/.vimrc
 rm ~/.vimperatorrc
 rm ~/.bash_profile
@@ -10,7 +10,6 @@ rm ~/.ctags
 rm ~/.gitconfig
 rm ~/.gitignore
 
-ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/vimperatorrc ~/.vimperatorrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
@@ -23,14 +22,10 @@ ln -s ~/dotfiles/global-gitignore ~/.gitignore
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-mkdir ~/.vim
-git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#if [ ! -d ~/.vim ]; then mkdir ~/.vim; fi
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim -c "VundleInstall"
-
-#cd ~/.vim/bundle/command-t/ruby/command-t
-#make clean
-#/usr/bin/ruby extconf.rb
-#make
 
 cd
