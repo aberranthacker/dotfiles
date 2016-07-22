a5c7751fb2469a0dc839bb34d335f6785c851c9f
Oleg Tsymbalyuk <randomizer@outlook.com>
2016-07-22 14:50:37 +0300

    install gruvbox colorscheme

--- install.sh
+++ install.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
+cd ~/dotfiles
+if [[ ! $(pwd) =~ "dotfiles" ]]; then exit; fi;
 
-rm ~/.vimrc
-rm ~/.vimperatorrc
-rm ~/.bash_profile
-rm ~/.bashrc
-rm ~/.bash_aliases
-rm ~/.tmux.conf
-rm ~/.ctags
-rm ~/.gitconfig
-rm ~/.gitignore
-
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/vimperatorrc ~/.vimperatorrc
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
-ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/ctags ~/.ctags
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/global-gitignore ~/.gitignore
+ln -sf dotfiles/vimrc ~/.vimrc
+ln -sf dotfiles/vimperatorrc ~/.vimperatorrc
+ln -sf dotfiles/bash_profile ~/.bash_profile
+ln -sf dotfiles/bash_aliases ~/.bash_aliases
+ln -sf dotfiles/bashrc ~/.bashrc
+ln -sf dotfiles/tmux.conf ~/.tmux.conf
+ln -sf dotfiles/ctags ~/.ctags
+ln -sf dotfiles/gitconfig ~/.gitconfig
+ln -sf dotfiles/global-gitignore ~/.gitignore
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-#if [ ! -d ~/.vim ]; then mkdir ~/.vim; fi
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim -c "VundleInstall"
 
+mkdir colorschemes
+cd colorschemes
+git clone https://github.com/morhetz/gruvbox.git
+git clone https://github.com/morhetz/gruvbox-contrib.git
+
+ln -sf ~/dotfiles/colorschemes/gruvbox-contrib/konsole/Gruvbox_dark.colorscheme ~/.local/share/konsole/Gruvbox_dark.colorscheme
+ln -sf ~/dotfiles/colorschemes/gruvbox-contrib/xresources/gruvbox-dark.xresources ~/.Xresources
+
+cp gruvbox/colors/gruvbox.vim ~/.vim/bundle/vim-colorschemes/colors/
+
 cd
+
