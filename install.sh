2d686188a269aa339de7a16e82ef0febfe53bb87
Oleg Tsymbalyuk <aberrant.hacker@gmail.com>
2018-02-14 19:57:33 +0300

    add spacemacs

--- install.sh
+++ install.sh
@@ -3,13 +3,21 @@ cd ~/dotfiles
 if [[ ! $(pwd) =~ "dotfiles" ]]; then exit; fi;
 
 ln -sf dotfiles/vimrc ~/.vimrc
+
+ln -sf dotfiles/_spacemacs ~/.spacemacs
+ln -sf dotfiles/spacemacs  ~/spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
 ln -sf dotfiles/vimperatorrc ~/.vimperatorrc
+
 ln -sf dotfiles/bash_profile ~/.bash_profile
 ln -sf dotfiles/bash_aliases ~/.bash_aliases
 ln -sf dotfiles/bashrc ~/.bashrc
 ln -sf dotfiles/inputrc ~/.inputrc
+
 ln -sf dotfiles/tmux.conf ~/.tmux.conf
 ln -sf dotfiles/ctags ~/.ctags
+
 ln -sf dotfiles/gitconfig ~/.gitconfig
 ln -sf dotfiles/global-gitignore ~/.gitignore
 
@@ -25,7 +33,5 @@ git submodule update --init
 
 ln -sf ~/dotfiles/colorschemes/gruvbox-contrib/konsole/Gruvbox_dark.colorscheme ~/.local/share/konsole/Gruvbox_dark.colorscheme
 ln -sf ~/dotfiles/colorschemes/gruvbox-contrib/xresources/gruvbox-dark.xresources ~/.Xresources
-
-cp gruvbox/colors/gruvbox.vim ~/.vim/bundle/vim-colorschemes/colors/
 
 cd
